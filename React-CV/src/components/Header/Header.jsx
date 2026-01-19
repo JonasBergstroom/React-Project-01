1049be0ff0464e62391f9393735c473412c7f2f9
Jonas Bergström <jontrian97@gmail.com>
2026-01-19 14:52:11 +0100

    Add Header with some style

---
+++ React-CV/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header>
+      <h1>My Resume</h1>
+          <p>
+        Jonas Bergström      
+      </p>
+    </header>
+  );
+}
