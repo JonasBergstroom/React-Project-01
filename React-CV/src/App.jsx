e72a374951efb03673ad765ab47f6f3fa1bb5331
Jonas Bergström <jontrian97@gmail.com>
2026-02-04 16:00:15 +0100

    Add CoreConcepts info rending with map

--- React-CV/src/App.jsx
+++ React-CV/src/App.jsx
@@ -1,13 +1,24 @@
 import Header from "./components/Header/Header"
+import { CORE_CONCEPTS } from "./data.js";
+import CoreConcept from "./components/CoreConcept.jsx";
+
 
 function App() {
 
   return (
     <div>
+      <Header></Header>
       <main>
-        <Header></Header>
         <section id="experience-info">
           <h2>My coding experience</h2>
+          <ul>
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept
+                kye={conceptItem.title}
+                {...conceptItem}
+              ></CoreConcept>
+            ))}
+          </ul>
         </section>
       </main>
     </div>
---
+++ React-CV/src/data.js
@@ -0,0 +1,18 @@
+export const CORE_CONCEPTS = [
+  {
+    title: "2019",
+    description: "info1",
+  },
+  {
+    title: "2020",
+    description: "info2",
+  },
+  {
+    title: "2021",
+    description: "info3",
+  },
+  {
+    title: "2022",
+    description: "info4",
+  },
+];
