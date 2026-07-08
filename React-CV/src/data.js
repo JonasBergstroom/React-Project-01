5e52af557d36c66c4fe4175a0a176c3c18533df8
Jonas Bergström <jontrian97@gmail.com>
2026-07-08 15:45:18 +0200

    Added images for each section

--- React-CV/src/data.js
+++ React-CV/src/data.js
@@ -1,17 +1,25 @@
+import componentsImg1 from "./assets/Screenshot 2026-02-04 at 15.37.24.png"
+import componentsImg2 from "./assets/Screenshot 2026-02-04 at 15.38.49.png"
+import componentsImg3 from "./assets/Screenshot 2026-02-04 at 15.39.37.png";
+import componentsImg4 from "./assets/Screenshot 2026-02-04 at 15.40.47.png";
 export const CORE_CONCEPTS = [
   {
+    image: componentsImg1,
     title: "2019",
     description: "info1",
   },
   {
+    image: componentsImg2,
     title: "2020",
     description: "info2",
   },
   {
+    image: componentsImg3,
     title: "2021",
     description: "info3",
   },
   {
+    image: componentsImg4,
     title: "2022",
     description: "info4",
   },
